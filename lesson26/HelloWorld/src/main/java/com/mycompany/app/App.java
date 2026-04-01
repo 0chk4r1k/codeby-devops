477808bba2f3c055a53c174b99449f091fc303bc
0chk4r1k <denis.gerblih@yandex.ru>
2026-04-01 18:04:04 +0300

    final test

--- lesson26/HelloWorld/src/main/java/com/mycompany/app/App.java
+++ lesson26/HelloWorld/src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World! Probably final test...";
+    private static final String MESSAGE = "Hello World! Final test!";
 
     public App() {}
 

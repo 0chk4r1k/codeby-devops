5ddb5a996ce037265872fcee7b3e66fdf36a4ff4
0chk4r1k <denis.gerblih@yandex.ru>
2026-04-01 18:07:46 +0300

    final test #2

--- lesson26/HelloWorld/src/test/java/com/mycompany/app/AppTest.java
+++ lesson26/HelloWorld/src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World! Final test!", app.getMessage());
+        assertEquals("Hello World! Final test #2!", app.getMessage());
     }
 }
